b6ed6069c855ac065be4c2b776e9dd7083b7cf41
DESKTOP-N9TM13V\zhumeilu <824434649@qq.com>
2017-05-03 14:31:19 +0800

    添加了一些ListView的优化，添加RecyclerView的使用

--- ListViewTest/app/src/main/java/com/example/zhumeilu/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/zhumeilu/listviewtest/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.zhumeilu.listviewtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.zhumeilu.listviewtest.model.Fruit;
 
@@ -11,19 +14,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    List<Fruit> fruitList = new ArrayList<>();
     private String[] data = new String[]{
             "abc", "zml", "maylor"
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,data);
 //        ListView listView = (ListView) findViewById(R.id.list_view);
 //        listView.setAdapter(arrayAdapter);
-        List<Fruit> fruitList = new ArrayList<>();
         Fruit fruit = null;
         fruit = new Fruit("苹果1",R.drawable.wechat);
         fruitList.add(fruit);
@@ -49,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
         fruitList.add(fruit);
         FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView  = (ListView) findViewById(R.id.list_view);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit temp = fruitList.get(position);
+                Toast.makeText(MainActivity.this, temp.getName(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
         listView.setAdapter(adapter);
     }
 }
