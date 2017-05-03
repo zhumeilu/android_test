45359ff7f88250aa746fabc6b4ab6e4a792dc103
zhumeilu <824434649@qq.com>
2017-05-03 16:19:02 +0800

    RecyclerView添加点击事件
    Signed-off-by: zhumeilu <824434649@qq.com>

---
+++ RecyclerViewTest/app/src/main/java/com/example/zhumeilu/recyclerviewtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.zhumeilu.recyclerviewtest;
+
+/**
+ * Created by zhumeilu on 2017/5/3.
+ */
+
+public class Fruit {
+    private String name;
+    private int id;
+
+    public Fruit(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
